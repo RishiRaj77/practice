a85a94015d63a3d17278a553e922c9ef8773dde2
RishiRaj77 <rishirajdubey72@gmail.com>
2025-08-05 14:16:38 +0530

    new

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+function addtocart(){
+    let items = [];
+    
+    return{
+        addItem(item){
+            items.push(item);
+            console.log(`Added: ${item}`);
+        },
+        getItems() {
+            return [...items]; // return a copy
+        }
+    }
+}
+
+const myCart = addtocart();
+myCart.addItem("Apple");
+myCart.addItem("Banana");
+
+console.log("Cart Contents:", myCart.getItems()); // ["Apple", "Banana"]
